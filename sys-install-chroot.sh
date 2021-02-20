e941abab1e37e6cfe9575861780f9642817843c0
Respirat0r <55364758+Respirat0r@users.noreply.github.com>
2021-02-20 20:40:20 +0400

    script

--- sys-install-chroot.sh
+++ sys-install-chroot.sh
@@ -33,7 +33,33 @@ passwd stalker
 echo "%wheel ALL=(ALL)ALL" > /etc/sudoers
 
 #apps
-pacman -S sudo nano lxdm cinnamon xorg gnome-extra xfce4-terminal arc-gtk-theme papirus-icon-theme nvidia-lts
+pacman -S sudo nano lxdm cinnamon gnome-extra xfce4-terminal arc-gtk-theme papirus-icon-theme nvidia-lts make flatpak snapd flameshot libreoffice qbittorrent gparted htop psensor
+
+#octopi
+git clone https://aur.archlinux.org/octopi.git
+cd octopi
+makepkg -si
+cd
+
+#pamac-all
+git clone https://aur.archlinux.org/pamac-all.git
+cd pamac-all
+makepkg -si
+cd
+
+#wallpaper
+git clone https://github.com/Respirat0r/sys-wallpaper.git
+cd sys-wallpaper
+./sys-wallpaper.sh
+
+#aur apps
+pamac install visual-studio-code-bin
+pamac install viber
+pamac install tela-icon-theme
 
 #entering
 systemctl enable lxdm
+
+#exit and reboot
+exit
+reboot
