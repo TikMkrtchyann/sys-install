f53afff25fc0b593c766afbb565fd08f54fbe726
stalker <55364758+Respirat0r@users.noreply.github.com>
2021-03-05 18:59:04 +0400

    file

---
+++ diskPartition.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#fdisk or cfdisk
+echo "fdisk[1] or cfdisk[2]?"
+read disk
+if [[ $disk == 1 ]]
+then
+    fdisk -l
+    echo "Enter your disks name: (example: /dev/sda, /dev/sdb)"
+    read diskName
+    fdisk $diskName
+elif
+    cfdisk
+else
+    echo "pashol nahui"
+
+
+
+# Boot Partitioning
+
+echo "Which is your boot partition? (example: /dev/sda1, /dev/sdb2)"
+read bootName
+mkfs.ext2 $bootName
+mount $bootName /boot
+
+# Root Partitioning
+
+echo "Which is your root partition? (example: /dev/sda1, /dev/sdb2)"
+read rootName
+mkfs.ext4 $rootName
+mount $rootName /mnt
+
+# Swap partitioning
+
+echo "Do you have swap partition? Yes[1], No[2]"
+read VAR
+if [[  $VAR == 1  ]] 
+then
+    echo "Which is your swap partition? (example: /dev/sda1, /dev/sdb2)"
+    read swapName
+    mkswap $swapName
+    swapon $swapName
+else
+    echo "Oki, let`s continue"
+fi
---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void timezoneFunc()
+{
+    char timezone;
+
+    printf("Which is your timezone: Armenia/Yerevan[1], another[2]?\n");
+    if(timezone == '1')
+        system("timedatectl set-timezone Asia/Yerevan");
+    else
+        printf("no)))\n");
+}
+
+void partitionFunc()
+{
+   system("chmod +x diskPartition.sh");
+   system("./diskPartition.sh");
+
+}
+
+int main()
+{
+    timezoneFunc();
+    partitionFunc();
+}
