6f9f792a634a6dd75552999cacb449c7bfcfa290
stalker <55364758+Respirat0r@users.noreply.github.com>
2021-03-06 15:52:51 +0400

    file

--- main.c
+++ main.c
@@ -9,9 +9,19 @@ void partitionFunc()
 
 }
 
+void pacstrapFunc()
+{
+	system("pacman -Sy");
+	system("pacstrap /mnt base linux-lts linux-firmware sudo nano grub dhcpcd networkmanager");	
+	system("genfstab -U /mnt >> /mnt/etc/fstab");
+	system("cp -r sys-install /mnt/home");
+	system("arch-chroot /mnt");
+}
+
 
 int main()
 {
-    system("timedatectl set-timezone Asia/Yerevan");
+    system("timedatectl set-ntp true");
     partitionFunc();
+    pacstrapFunc();
 }
