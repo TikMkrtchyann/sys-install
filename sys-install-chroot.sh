6155ca67d3da294b195f41efe90ef4209b11522b
stalker <55364758+Respirat0r@users.noreply.github.com>
2021-03-06 18:21:41 +0400

    Update sys-install-chroot.sh

--- sys-install-chroot.sh
+++ sys-install-chroot.sh
@@ -1,5 +1,5 @@
 #time in chroot
-echo "Enter your timezone: (example: Asia/Yerevan, Europe/Paris)
+echo Enter your timezone: example: Asia/Yerevan, Europe/Paris
 read timezone
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
@@ -10,15 +10,12 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 #host/hostname
-echo "Enter your hostname: "
+echo Enter your hostname: 
 read hostName
 echo $hostName > /etc/hostname
-echo "127.0.0.1 localhost
-::1 localhost
-127.0.1.1 $hostName.localdomain $hostName"
 
 #create sudo user
-echo "Enter your root-user password: ")
+echo Enter your root-user password: 
 passwd
 
 #internet
@@ -27,61 +24,59 @@ systemctl enable NetworkManager.service
 
 
 #grub
-echo "In what disk you want to install GRUB? (example: /dev/sda, /dev/sdc"
+echo In what disk you want to install GRUB? example: /dev/sda, /dev/sdc
 read diskName
 grub-install $diskName
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #add user and password
-echo "Enter your username: "
+echo Enter your username: 
 read userName
 useradd -m -g users -G wheel -s /bin/bash $userName
 passwd $userName
 echo "%wheel ALL=(ALL)ALL" > /etc/sudoers
 
 #apps
-echo "What DE(Desktop Environment) Do you want to install? Cinnamon[1], XFCE[2], KDE Plasma[3]"
+echo What DE-Desktop Environment Do you want to install? Cinnamon[1], XFCE[2], KDE Plasma[3]
 read DE
 if [[ $DE == 1 ]]; then
-pacman -S cinnamon 
+	pacman -S cinnamon 
 
-elif[[ $DE == 2 ]]; then
-pacman -S xfce4 
+elif [[ $DE == 2 ]]; then
+	pacman -S xfce4 
 
-elif[[ $DE == 3 ]]; then
-pacman -S plasma plasma-meta
+elif [[ $DE == 3 ]]; then
+	pacman -S plasma plasma-meta
 
-echo "What Display Manager Do you want to install? LXDM[1], SDDM[2], GDM[3], LightDM[4]"
+echo What Display Manager Do you want to install? LXDM[1], SDDM[2], GDM[3], LightDM[4]
 read DM 
 if [[ $DM == 1 ]]; then
-pacman -S lxdm
-systemctl enable lxdm
+	pacman -S lxdm
+	systemctl enable lxdm
 
-elif[[ $DM == 2 ]]; then
-pacman -S sddm
-systemctl enable sddm
+elif [[ $DM == 2 ]]; then
+	pacman -S sddm
+	systemctl enable sddm
 
-elif[[ $DM == 3 ]]; then
-pacman -S gdm
-systemctl enable gdm
+elif [[ $DM == 3 ]]; then
+	pacman -S gdm
+	systemctl enable gdm
 
-elif[[ $DM == 4 ]]; then
-pacman -S lightdm
-systemctl enable lightdm
+elif [[ $DM == 4 ]]; then
+	pacman -S lightdm
+	systemctl enable lightdm
 
-echo "What terminal Do you want to install? Gnome-terminal[1], xfce4-terminal[2], konsole[3]"
+echo What terminal Do you want to install? Gnome-terminal[1], xfce4-terminal[2], konsole[3]
 read TERM
 if [[ $TERM == 1 ]]; then
-pacman -S gnome-terminal
+	pacman -S gnome-terminal
 
 elif [[ $TERM == 2 ]]; then
-pacman -S xfce4-terminal
+	pacman -S xfce4-terminal
 
 elif [[ $TERM == 3 ]]; then
-pacman -S konsole
+	pacman -S konsole
 
-echo "Now, I say you goodbye my friend. I helped you to install this Linux and maybe, I will help you then. See you soon)) "
+echo Now, I say you goodbye my friend. I helped you to install this Linux and maybe, I will help you then. See you soon
 read $a
 
-exit
-reboot
