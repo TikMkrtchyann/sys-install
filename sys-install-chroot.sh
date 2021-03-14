7ebda0a12d4a59d2484d0abc1954f64a93658181
stalker <55364758+Respirat0r@users.noreply.github.com>
2021-03-14 15:11:46 +0400

    Update sys-install-chroot.sh

--- sys-install-chroot.sh
+++ sys-install-chroot.sh
@@ -10,12 +10,12 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 #host/hostname
-echo Enter your hostname: 
+echo Enter your hostname:
 read hostName
 echo $hostName > /etc/hostname
 
 #create sudo user
-echo Enter your root-user password: 
+echo Enter your root-user password:
 passwd
 
 #internet
@@ -30,7 +30,7 @@ grub-install $diskName
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #add user and password
-echo Enter your username: 
+echo Enter your username:
 read userName
 useradd -m -g users -G wheel -s /bin/bash $userName
 passwd $userName
@@ -40,17 +40,17 @@ echo "%wheel ALL=(ALL)ALL" > /etc/sudoers
 echo What DE-Desktop Environment Do you want to install? Cinnamon[1], XFCE[2], KDE Plasma[3]
 read DE
 if [[ $DE == 1 ]]; then
-	pacman -S cinnamon 
+	pacman -S cinnamon
 
 elif [[ $DE == 2 ]]; then
-	pacman -S xfce4 
+	pacman -S xfce4
 
 elif [[ $DE == 3 ]]; then
 	pacman -S plasma plasma-meta
 fi
 
 echo What Display Manager Do you want to install? LXDM[1], SDDM[2], GDM[3], LightDM[4]
-read DM 
+read DM
 if [[ $DM == 1 ]]; then
 	pacman -S lxdm
 	systemctl enable lxdm
